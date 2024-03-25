6ee7bb99bac691c94a8118af4c59fd39a087c05c
riddhitandel <riddhi.tandel@dcmail.ca>
2024-03-25 15:19:17 -0400

    Create main.js
    VARIABLE AND FUNCTION DEFINITIONS

---
+++ main.js
@@ -0,0 +1,10 @@
+// variable and function definitions 
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
