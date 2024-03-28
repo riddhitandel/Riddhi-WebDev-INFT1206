c82d250d2045c32f99a55c227bae943c6779a6e9
riddhitandel <riddhi.tandel@dcmail.ca>
2024-03-28 17:14:44 -0400

    Update main2.js

--- main2.js
+++ main2.js
@@ -8,12 +8,27 @@ const overlay = document.querySelector('.overlay');
 const imageFilesnames = ["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"]
 
 /* Declaring the alternative text for each image file */
-
+const imageAltText = {
+    'pic1.jpg': 'Description for pic1',
+    'pic2.jpg': 'Description for pic2',
+    'pic3.jpg': 'Description for pic3',
+    'pic4.jpg': 'Description for pic4',
+    'pic5.jpg': 'Description for pic5'
+  };
+  
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+imageSet.forEach(fileName => {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', xxx);
+    newImage.setAttribute('alt', xxx);
+    thumbBar.appendChild(newImage);
+})
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    if (overlay.style.backgroundColor === `rgba(0, 0, 0, 0)` || overlay.style.backgroundColor === ``) {
+       overlay.style.backgroundColor = `rgba(200, 200, 200, 1)`;
+    } else {
+       overlay.style.backgroundColor = `rgba(0, 0, 0, 0)`;
+    }
+ });
