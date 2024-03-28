de26541baa69063bc8864880ab8712499a1334bd
riddhitandel <riddhi.tandel@dcmail.ca>
2024-03-28 17:51:24 -0400

    Update main2.js

--- main2.js
+++ main2.js
@@ -5,7 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imageFilesnames = ["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"]
+const imageFilesnames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 const imageAltText = {
@@ -15,14 +15,22 @@ const imageAltText = {
     'pic4.jpg': 'Description for pic4',
     'pic5.jpg': 'Description for pic5'
   };
-  
+
+
 /* Looping through images */
-imageSet.forEach(fileName => {
+imageFilesnames.forEach(fileName => {
     const newImage = document.createElement('img');
-    newImage.setAttribute('src', xxx);
-    newImage.setAttribute('alt', xxx);
+    newImage.setAttribute('src', `images/${fileName}` );
+    newImage.setAttribute('alt', imageAltText[fileName]);
+    // append the image to thumb-bar
     thumbBar.appendChild(newImage);
-})
+   
+    // add click event listener to each thumbnail iimage 
+    newImage.addEventListener('click',function(){
+        displayedImage.src = this.src;
+        displayedImage.alt = this.alt;
+    });  
+});
 
 /* Wiring up the Darken/Lighten button */
 btn.addEventListener('click', () => {
