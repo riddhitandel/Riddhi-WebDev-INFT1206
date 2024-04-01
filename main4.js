24bfbae0db315f31b11425e7902f2498a8dd11b1
riddhitandel <riddhi.tandel@dcmail.ca>
2024-04-01 11:41:39 -0400

    Update main4.js
    adding condition in collision detection

--- main4.js
+++ main4.js
@@ -59,11 +59,11 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
-
+  
         if (distance < this.size + ball.size) {
           ball.color = this.color = randomRGB();
         }
